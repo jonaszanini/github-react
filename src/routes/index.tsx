a2d0527e0baa2fec1e5e48d1b2ebe80228f500c3
Jonas Zanini <jonaszanini@gmail.com>
2020-12-19 11:37:13 -0500

    routers created

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Repository from '../pages/repository';
+import Dashboard from '../pages/dashboard';
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={Dashboard} />
+    <Route path="/repository" component={Repository} />
+  </Switch>
+);
+
+export default Routes;
